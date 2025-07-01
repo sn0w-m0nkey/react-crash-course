e33d7977322af8911341c5ec4f4a2c0f714248aa
sn0w-m0nkey <148561536+sn0w-m0nkey@users.noreply.github.com>
2025-07-01 17:44:08 +0100

    Updating objects in state

--- src/App.js
+++ src/App.js
@@ -1,70 +1,92 @@
+import { useState } from 'react'
 import { Fragment } from 'react'
 import { ProductList } from './components/ProductList'
 import { ProductCard } from './components/ProductCard'
-import './App.css'
+import { ProductFilter } from './components/ProductFilter'
+import styles from './App.module.css'
 
 function App() {
   const products = [
     {
-      imgSrc: 'images/iphone.png',
+      imageSrc: 'images/iphone.png',
       title: 'iPhone 15 Pro',
-      specificiation: [
+      specification: [
         'A17 Pro chip with 6-core GPU',
         '3x or 5x Telephoto camera',
         'Up to 29 hours video playback',
       ],
-      price: 999,
       stockCount: 10,
+      price: 999,
     },
     {
-      imgSrc: 'images/airpods.png',
-      title: 'Airpods Pro 2',
-      specificiation: [
+      imageSrc: 'images/airpods.png',
+      title: 'AirPods Pro 2',
+      specification: [
         'Noise Cancellation',
-        'Dust, sweat and water resistant',
-        'Up to 6 hours of listening time',
+        'Dust, sweat, and water resistant',
+        'Up to 6 hours of listening',
       ],
-      price: 249,
       stockCount: 0,
+      price: 249,
     },
     {
-      imgSrc: 'images/apple-watch.png',
+      imageSrc: 'images/apple-watch.png',
       title: 'Apple Watch 9',
-      specificiation: [
+      specification: [
         '45mm or 41mm case size',
         'Always-On Retina display',
         'Up to 18 hours normal use',
       ],
-      price: 399,
       stockCount: 6,
+      price: 399,
     },
   ]
+  const [filters, setFilters] = useState({
+    price: {
+      min: 0,
+      max: 999,
+    },
+    other: 'other value',
+  })
 
   function handlePurchase(product) {
     alert(`You clicked on ${product.title} which cost $${product.price}`)
   }
 
+  function handleFilter(key, value) {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      price: {
+        ...prevFilters.price,
+        [key]: value,
+      },
+    }))
+  }
+
   return (
-    <div className="App">
+    <div className={styles.App}>
       <ProductList>
         {products.map((product) => (
           <ProductCard
+            key={product.title}
             product={product}
             onPurchase={handlePurchase}
-            key={product.title}
           />
         ))}
       </ProductList>
 
-      <h2>Products which cost up to $500</h2>
-
+      <h2>Products filtered by price</h2>
+      <ProductFilter filters={filters} onFilter={handleFilter} />
       {products
-        .filter(({ price }) => price < 500)
+        .filter(
+          ({ price }) =>
+            price >= filters.price.min && price <= filters.price.max
+        )
         .map(({ title, price }) => (
           <Fragment key={title}>
-            <hr className="ListDivider" />
-            <p className="ListTitle">
-              {title} - ${price}
+            <hr className={styles.ListDivider} />
+            <p className={styles.ListTitle}>
+              {title} cost ${price}
             </p>
           </Fragment>
         ))}
